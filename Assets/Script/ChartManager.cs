a84f611bff07ef16cda1d9ddb6e7341f6cea2adf
ChickenPige0n <2336983354@qq.com>
2023-10-25 18:21:44 +0800

    refactor: seperate ChartData class

--- Assets/Script/ChartManager.cs
+++ Assets/Script/ChartManager.cs
@@ -27,10 +27,10 @@ namespace Phigodot.Game
 		public Label DiffLabel;
 
 		[Export]
-		public Label GameModeInfo;
+		public Label GameModeLabel;
 
 		[Export]
-		public Label ComboInfo;
+		public Label ComboLabel;
 		public int Combo;
 		public int Time;
 
@@ -41,75 +41,6 @@ namespace Phigodot.Game
 		public AudioStreamPlayer Music;
 
 
-
-        public partial class ChartData
-        {
-            //曲绘绝对路径
-            public string ImageSource { get; set; }
-            public string MusicFileName { get; set; }
-            public string ChartFileName { get; set; }
-            
-            public string ChartName { get; set; }
-            public string ChartDiff { get; set; }
-            public string ChartPath { get; set; }
-
-            public string Composer { get; set; }
-            public string Charter { get; set; }
-
-            public string Illustrator { get; set; }
-
-			public ChartData()
-        	{
-        	    ImageSource   = string.Empty;
-        	    MusicFileName = string.Empty;
-        	    ChartFileName = string.Empty;
-        	    ChartName     = string.Empty;
-        	    ChartDiff     = string.Empty;
-        	    ChartPath     = string.Empty;
-        	    Composer      = string.Empty;
-        	    Charter       = string.Empty;
-        	    Illustrator   = string.Empty;
-        	}
-			public static ChartData FromString(string RootDir, string infoContent)
-			{
-				ChartData cd = new ChartData();
-            	string[] lines = infoContent.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-            	foreach (string line in lines)
-            	{
-            	    string[] infos = line.Split(new[] { ':' });
-            	    switch (infos[0].Trim())
-            	    {
-            	        case "Name":
-            	            cd.ChartName = infos[1].Trim();
-            	            break;
-            	        case "Path":
-            	            cd.ChartPath = infos[1].Trim();
-            	            break;
-            	        case "Picture":
-            	            cd.ImageSource = Path.Combine(RootDir,infos[1].Trim());
-            	            break;
-            	        case "Level":
-            	            cd.ChartDiff = infos[1].Trim();
-            	            break;
-            	        case "Song":
-            	            cd.MusicFileName = infos[1].Trim();
-            	            break;
-            	        case "Chart":
-            	            cd.ChartFileName = infos[1].Trim();
-            	            break;
-            	        case "Composer":
-            	            cd.Composer = infos[1].Trim();
-            	            break;
-            	        case "Charter":
-            	            cd.Charter = infos[1].Trim();
-            	            break;
-            	        default:
-            	            break;
-            	    }
-            	}
-				return cd;
-			}
-		}
 
 		public bool LoadChart(string RootDir)
 		{
@@ -133,24 +64,24 @@ namespace Phigodot.Game
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
-			GameModeInfo.Text = "Autoplay";
+			GameModeLabel.Text = "Autoplay";
 			bool loadSuccess = LoadChart("res://Assets/ExampleChart/35461163/");
 		}
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
 		public override void _Process(double delta)
 		{
-			ComboInfo.Text = Combo.ToString();
+			ComboLabel.Text = Combo.ToString();
 			ScoreLabel.Text = Score.ToString("D7");
 
 			if (Combo >= 3)
 			{
-				GameModeInfo.Visible = true;
-				ComboInfo.Visible = true;
+				GameModeLabel.Visible = true;
+				ComboLabel.Visible = true;
 			}
 			else
 			{
-				GameModeInfo.Visible = false;
-				ComboInfo.Visible = false;
+				GameModeLabel.Visible = false;
+				ComboLabel.Visible = false;
 			}
 		}
 	}
